b64e25235fec6d3fd46b231624927ce65437402f
cvigueras <carlosj.vigueras@gmail.com>
2023-04-12 12:21:11 +0200

    Feat: added calculator class

---
+++ RomanNumerals.Console/Calculator.cs
@@ -0,0 +1,21 @@
+namespace RomanNumerals.Console;
+
+public class Calculator
+{
+    private readonly Dictionary<int, string> _romanNumerals;
+
+    public Calculator()
+    {
+        _romanNumerals = new Dictionary<int, string>
+        {
+            {1, "I"},
+            {2, "II"},
+            {3, "III"}
+        };
+    }
+    public string GetRomanNumber(int number)
+    {
+        var result = _romanNumerals.FirstOrDefault(x=> x.Key == number).Value;
+        return string.IsNullOrEmpty(result) ? string.Empty : result;
+    }
+}
