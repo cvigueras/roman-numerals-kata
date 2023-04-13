2f2c5bb5dc9122b1bc9ff0ed84be4ee35e4ed09e
cvigueras <carlosj.vigueras@gmail.com>
2023-04-13 10:54:10 +0200

    Refactor: extract methods in production code.

--- RomanNumerals.Console/Calculate.cs
+++ RomanNumerals.Console/Calculate.cs
@@ -19,14 +19,36 @@ public class Calculate
     private string FormatUnitsRomanNumber(ArabicNumber number)
     {
         var closestNumber = _romanNumerals.GetClosestNumber(number.Value);
-        if (closestNumber > number.Value)
+        if (IsClosestUpToNumber(number, closestNumber))
         {
-            if (Math.Abs(number.Value - closestNumber) == 1)
+            if (IsSubtract(number, closestNumber))
             {
-                return _romanNumerals.SearchRomanNumeralValue(number.Value - closestNumber) + _romanNumerals.SearchRomanNumeralValue(closestNumber);
+                return FormatUnitsSubtract(number, closestNumber);
             }
             closestNumber = _romanNumerals.GetPreviousClosestNumber(number.Value);
         }
-        return _romanNumerals.SearchRomanNumeralValue(closestNumber) + _romanNumerals.SearchRomanNumeralValue(number.Value - closestNumber);
+        return FormatUnitsSum(number, closestNumber);
+    }
+
+    private string FormatUnitsSum(ArabicNumber number, int closestNumber)
+    {
+        return _romanNumerals.SearchRomanNumeralValue(closestNumber) +
+               _romanNumerals.SearchRomanNumeralValue(number.Value - closestNumber);
+    }
+
+    private string FormatUnitsSubtract(ArabicNumber number, int closestNumber)
+    {
+        return _romanNumerals.SearchRomanNumeralValue(number.Value - closestNumber) +
+               _romanNumerals.SearchRomanNumeralValue(closestNumber);
+    }
+
+    private static bool IsSubtract(ArabicNumber number, int closestNumber)
+    {
+        return Math.Abs(number.Value - closestNumber) == 1;
+    }
+
+    private bool IsClosestUpToNumber(ArabicNumber number, int closestNumber)
+    {
+        return closestNumber > number.Value;
     }
 }
